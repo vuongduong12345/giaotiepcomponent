f34151203c3b2a22bb3b34b27d2ce02a7507540f
vuongduong12345 <vuongtatduong@gmail.com>
2019-12-30 16:00:10 +0700

    Create Parent.vue

---
+++ src/components/Parent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <Child title="toi ten la Duong"/>
+  </div>
+</template>
+
+<script>
+import Child from './Child'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+      Child
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
