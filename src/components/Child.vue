cd1b0738d72b55323a3af8f4b45124d80a4949dc
vuongduong12345 <vuongtatduong@gmail.com>
2019-12-30 16:00:05 +0700

    Create Child.vue

---
+++ src/components/Child.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{test}}</h1>
+        <h1>{{title}}</h1>
+        
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        title: String
+    },
+    data(){
+        return {
+            test:'toi là thăng con'
+        }
+    }
+    
+}
+</script>
+<style>
+
+</style>
